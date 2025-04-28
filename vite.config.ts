06327790b49972abf4706bba937aa04863117ad6
Pronay Guha <pronay.guha@thermofisher.com>
2025-04-28 18:05:50 +0530

    setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@console": path.resolve(__dirname, "./src/Console"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@editor": path.resolve(__dirname, "./src/Editor"),
+      "@explorer": path.resolve(__dirname, "./src/Explorer"),
+      "@header": path.resolve(__dirname, "./src/Header"),
+      "@types": path.resolve(__dirname, "./src/types"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
+    },
+  },
+});
