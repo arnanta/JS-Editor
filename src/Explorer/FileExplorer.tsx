381d48043a4cf897a6d9dbffa6ac504620153470
Arnanta Chatterjee <arnantachatterjee000@gmail.com>
2025-04-29 11:19:55 +0530

    Explorer folder structure mapped to show in UI, WIP new file adddition

---
+++ src/Explorer/FileExplorer.tsx
@@ -0,0 +1,90 @@
+import React, { useState } from 'react';
+import style from './styles/FileExplorer.module.css';
+import { folderStructure } from './FolderStructure';
+import {
+  NewFile,
+  AddFile,
+  Folder as FolderIcon,
+  CollapseAll,
+  ExplorerIcon,
+} from '../assets/icons';
+
+interface FileNode {
+  type: 'file' | 'folder';
+  name: string;
+  children?: FileNode[];
+}
+
+const Folder = ({ item }: { item: FileNode }) => {
+  const [expanded, setExpanded] = useState(false);
+  const [selectedNode, setSelectedNode] = useState<FileNode | null>(null);
+  const [fileTree, setFileTree] = useState<FileNode[]>();
+  //! Not needed
+  if (item.type === 'file') {
+    return <div className={style.file}>{item.name}</div>;
+  }
+
+  const handleFolderClick = () => {
+    setExpanded(!expanded);
+    setSelectedNode(item.name);
+  };
+
+  return (
+    <div className={style.folder}>
+      <div className={style.folderName} onClick={handleFolderClick}>
+        {expanded ? '📂' : '📁'} {item.name}
+      </div>
+
+      {expanded && item.children && (
+        <div className={style.children}>
+          {item.children.map((child, index) => (
+            <Folder key={index} item={child} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+const FileExplorer = () => {
+  const [isCreatingNew, setIsCreatingNew] = useState(false);
+  const handleCreateNewFile = (fileName: string) => {
+    console.log('🚀 ~ handleCreateNewFile ~ fileName:', fileName);
+    //! Will add the file with the name and type file to the list of folders creating a copy of it
+  };
+  return (
+    <div className={style.explorer}>
+      {/* Toolbar */}
+      <div className={style.toolbar}>
+        <ExplorerIcon />
+        <NewFile onClick={() => setIsCreatingNew(true)} />
+        <AddFile />
+        <FolderIcon />
+        <CollapseAll />
+      </div>
+      {isCreatingNew && (
+        <div className="pl-4">
+          <input
+            autoFocus
+            type="text"
+            className="bg-gray-700 text-white px-1 py-0.5"
+            placeholder="new file name"
+            onKeyDown={(e) => {
+              if (e.key === 'Enter' && e.currentTarget.value) {
+                handleCreateNewFile(e.currentTarget.value);
+                setIsCreatingNew(false);
+              }
+            }}
+            onBlur={() => setIsCreatingNew(false)}
+          />
+        </div>
+      )}
+      {/* File Structure */}
+      <div className={style.verticalLine}>
+        <Folder item={folderStructure} />
+      </div>
+    </div>
+  );
+};
+
+export default FileExplorer;
