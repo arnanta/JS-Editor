d916517ed00ad5e621057b9238f9eeeca4a8a811
Arnanta Chatterjee <arnantachatterjee000@gmail.com>
2025-05-06 23:05:20 +0530

    icons changes

--- src/assets/icons.tsx
+++ src/assets/icons.tsx
@@ -105,7 +105,7 @@ export const Folder = () => (
     width="24px"
     fill="#e3e3e3"
   >
-    <path d="M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h240l80 80h320q33 0 56.5 23.5T880-640v400q0 33-23.5 56.5T800-160H160Z" />
+    <path d="M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h240l80 80h320q33 0 56.5 23.5T880-640v400q0 33-23.5 56.5T800-160H160Zm0-80h640v-400H447l-80-80H160v480Zm0 0v-480 480Z" />
   </svg>
 );
 
@@ -213,6 +213,17 @@ export const AddIcon = () => (
     <path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z" />
   </svg>
 );
+export const FolderOpen = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="24px"
+    viewBox="0 -960 960 960"
+    width="24px"
+    fill="#e3e3e3"
+  >
+    <path d="M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h240l80 80h320q33 0 56.5 23.5T880-640H447l-80-80H160v480l96-320h684L837-217q-8 26-29.5 41.5T760-160H160Zm84-80h516l72-240H316l-72 240Zm0 0 72-240-72 240Zm-84-400v-80 80Z" />
+  </svg>
+);
 // Export all icons as named exports
 export default {
   ChevronDown,
@@ -231,4 +242,5 @@ export default {
   ChevronRight,
   DeleteIcon,
   AddIcon,
+  FolderOpen,
 };
